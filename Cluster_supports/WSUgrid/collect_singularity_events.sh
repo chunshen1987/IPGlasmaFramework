438be8c3462d7722646f5dd06bed5d692c0268ec
Chun Shen <chunshen1987@gmail.com>
2021-09-21 22:33:40 -0400

    improve the collecting script

--- Cluster_supports/WSUgrid/collect_singularity_events.sh
+++ Cluster_supports/WSUgrid/collect_singularity_events.sh
@@ -28,7 +28,6 @@ cp ${fromFolder}/parameter* ${target_folder}/
 target_res_folder=${target_folder}/RESULTS
 mkdir -p ${target_res_folder}
 
-total_eventNum=0
 collected_eventNum=0
 for ijob in `ls --color=none $fromFolder | grep "event" `;
 do
@@ -43,9 +42,14 @@ done
 
 echo "Collected events number: " $collected_eventNum
 
+if [ "$collected_eventNum" -eq "0" ]; then
+    exit 0
+fi
+
 if [ -f ${target_folder}/${folderName}.h5 ]; then
     mv ${target_folder}/${folderName}.h5 ${target_res_folder}
 fi
+rm -fr RESULTS.h5
 ./combine_multiple_hdf5.py ${target_res_folder}
 mv RESULTS.h5 ${target_folder}/${folderName}.h5
 rm -fr $target_res_folder
--- utilities/collect_events.sh
+++ utilities/collect_events.sh
@@ -8,12 +8,12 @@ toFolder=$2
 if [ -z "$fromFolder" ]
 then
     echo $usage
-    exit 1
+    exit 0
 fi
 if [ -z "$toFolder" ]
 then
     echo $usage
-    exit 1
+    exit 0
 fi
 
 fromFolder=${fromFolder%"/"}
@@ -29,7 +29,6 @@ cp -r ${fromFolder}/model_parameters ${target_folder}/
 target_res_folder=${target_folder}/RESULTS
 mkdir -p ${target_res_folder}
 
-total_eventNum=0
 collected_eventNum=0
 for ijob in `ls --color=none $fromFolder | grep "event" `;
 do
@@ -44,9 +43,14 @@ done
 
 echo "Collected events number: " $collected_eventNum
 
+if [ "$collected_eventNum" -eq "0" ]; then
+    exit 0
+fi
+
 if [ -f ${target_folder}/${folderName}.h5 ]; then
     mv ${target_folder}/${folderName}.h5 ${target_res_folder}
 fi
+rm -fr RESULTS.h5
 ./combine_multiple_hdf5.py ${target_res_folder}
 mv RESULTS.h5 ${target_folder}/${folderName}.h5
 rm -fr $target_res_folder
