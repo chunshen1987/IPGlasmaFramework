5fc47da415ce8a73d6870907d5dab8c1a8ffc3c9
Chun Shen <chunshen1987@gmail.com>
2022-07-08 10:31:52 -0400

    take out the amplitudelib_v2

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -9,9 +9,6 @@ git clone --depth=1 https://github.com/chunshen1987/ipglasma ipglasma_code
 rm -fr ipglasma_code/.git
 
 # download subnucleondiffraction
-rm -fr amplitudelib_v2
-git clone --depth=1 https://github.com/hejajama/amplitudelib amplitudelib_v2
-rm -fr amplitudelib_v2/.git
 rm -fr subnucleondiffraction_code
 git clone --depth=1 https://github.com/hejajama/subnucleondiffraction subnucleondiffraction_code
 rm -fr subnucleondiffraction_code/.git
