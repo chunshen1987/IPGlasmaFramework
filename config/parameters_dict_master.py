d34d2cf558fe504a659e9907119f4690c3fc5333
Wenbin <1501110084@pku.edu.cn>
2024-05-28 14:36:23 -0700

    Don't do 3D rotation for omega!=1

--- config/parameters_dict_master.py
+++ config/parameters_dict_master.py
@@ -39,7 +39,7 @@ ipglasma_dict = {
     'BGq': 0.3,
     'BGqVar': 0.0,
     'omega': 1.0, 
-    'dqMin': 0.0,
+    'dqMin': 0.2582,
     'useSmoothNucleus': 0,
     'useConstituentQuarkProton': 0,
     'NqFluc': 0.0,
