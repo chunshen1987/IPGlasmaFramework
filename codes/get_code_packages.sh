3968ba68716e3e78190a20f081505898d7c83f01
Chun Shen <chunshen@wayne.edu>
2025-02-10 09:39:40 -0500

    update the ipglasma version

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -5,7 +5,7 @@
 # download IPGlasma
 rm -fr ipglasma_code
 git clone --depth=1 https://github.com/chunshen1987/ipglasma -b ipglasma_jimwlk ipglasma_code
-(cd ipglasma_code; git checkout aa1553ce199669dcda467d8677f055403c72314e)
+(cd ipglasma_code; git checkout e3e7b1528554df04b062fc88cda721d5b78a09a6)
 rm -fr ipglasma_code/.git
 
 # download subnucleondiffraction
