8d533a0974f98b5ab440a6103bcb3d7ae706702a
Wenbin <1501110084@pku.edu.cn>
2025-04-25 10:59:16 -0700

    Merge with Chun branch

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -4,8 +4,8 @@
 
 # download IPGlasma
 rm -fr ipglasma_code
-git clone --depth=1 https://github.com/wenbin1501110084/ipglasma -b stingy_proton_jimwlk ipglasma_code
-#(cd ipglasma_code; git checkout 492e80291b2e68d3bc28644bf524e59dc1af9c7d)
+git clone --depth=1 https://github.com/chunshen1987/ipglasma -b ipglasma_jimwlk ipglasma_code
+(cd ipglasma_code; git checkout d1b46fa98d4e39a288bfde887b9d196d648cfc60)
 rm -fr ipglasma_code/.git
 
 # download subnucleondiffraction
@@ -14,4 +14,3 @@ rm -fr subnucleondiffraction_code
 git clone --depth=1 https://github.com/chunshen1987/subnucleondiffraction -b integratedCrossSection subnucleondiffraction_code
 (cd subnucleondiffraction_code; git checkout 8eaefcd5a4ea90eef1c5fbe4edb6604c5c1f2b9a)
 rm -fr subnucleondiffraction_code/.git
-
