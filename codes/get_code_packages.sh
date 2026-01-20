0f91328b5a76a8288735b72f46f64721f1f2b551
Hendrik Roch <hroch@physik.uni-bielefeld.de>
2026-01-20 16:51:17 +0200

    Fix mistake in testing setup

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -11,13 +11,9 @@ rm -fr ipglasma_code/.git
 # download subnucleondiffraction
 rm -fr subnucleondiffraction_code
 #git clone --depth=1 https://github.com/hejajama/subnucleondiffraction subnucleondiffraction_code
-
-#git clone --depth=1 https://github.com/chunshen1987/subnucleondiffraction subnucleondiffraction_code
-#(cd subnucleondiffraction_code; git checkout 3ce967afdda1d0eb2eb83ac13b563a65c38941b3)
-
-git clone https://github.com/chunshen1987/subnucleondiffraction subnucleondiffraction_code
-(cd subnucleondiffraction_code; git checkout hendrik_dev)
-#rm -fr subnucleondiffraction_code/.git
+git clone --depth=1 https://github.com/chunshen1987/subnucleondiffraction subnucleondiffraction_code
+(cd subnucleondiffraction_code; git checkout roch/fix_sigmatot_inc)
+rm -fr subnucleondiffraction_code/.git
 
 # download nucleus configurations for IP-Glasma
 (cd ipglasma_code/nucleusConfigurations; bash download_nucleusTables.sh;)
